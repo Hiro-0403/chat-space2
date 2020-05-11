acfcc61c68361199b879c8414797fb299d31bfb3
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-05-11 20:57:53 +0900

    最新

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  #下の行は削除する
-  #root "messages#index"
+  root 'groups#index'
 
   root "messages#index"
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
